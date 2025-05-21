e51e24fab7ea0c4c1ff35f64b37254575b6f0ff8
Joao Lucas <jldecristo@gmail.com>
2025-05-21 00:23:20 -0300

    Classe Game normal

---
+++ src/main/java/com/jldeveloper/dslist/entities/Game.java
@@ -0,0 +1,117 @@
+package com.jldeveloper.dslist.entities;
+
+import java.util.Objects;
+
+public class Game {
+    private Long id;
+    private String title;
+    private Integer year;
+    private String genre;
+    private String platform;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public Game() {
+    }
+
+    public Game(String longDescription, String shortDescription,
+                String imgUrl, Double score, String platform,
+                String genre, Integer year, String title, Long id) {
+        this.longDescription = longDescription;
+        this.shortDescription = shortDescription;
+        this.imgUrl = imgUrl;
+        this.score = score;
+        this.platform = platform;
+        this.genre = genre;
+        this.year = year;
+        this.title = title;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return Objects.equals(id, game.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
