d3979ce7eec86686b8c0ae91a3df6f179bdb5fe1
Joao Lucas <jldecristo@gmail.com>
2025-05-21 21:24:47 -0300

    correção da tabela long

--- src/main/java/com/jldeveloper/dslist/entities/Game.java
+++ src/main/java/com/jldeveloper/dslist/entities/Game.java
@@ -20,6 +20,8 @@ public class Game {
     private Double score;
     private String imgUrl;
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
