5deaf05d6326981cc51a2277fc097279a8f4bc95
Joao Lucas <jldecristo@gmail.com>
2025-05-22 00:00:58 -0300

    variáveis e construtores do GameMinDTO

--- src/main/java/com/jldeveloper/dslist/dto/GameMinDTO.java
+++ src/main/java/com/jldeveloper/dslist/dto/GameMinDTO.java
@@ -1,4 +1,24 @@
 package com.jldeveloper.dslist.dto;
 
+import jakarta.persistence.Column;
+
 public class GameMinDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameMinDTO() {
+
+    }
+
+    public GameMinDTO(Game entity) {
+        id = entity.getId();
+        title = entity.geTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+    }
 }
