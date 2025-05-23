6e4ff1da6d1c93ffc1d93e2d531e8c9aac770803
Joao Lucas <jldecristo@gmail.com>
2025-05-22 21:26:00 -0300

    lista que retorna GameMinDTO

--- src/main/java/com/jldeveloper/dslist/services/GameService.java
+++ src/main/java/com/jldeveloper/dslist/services/GameService.java
@@ -1,8 +1,15 @@
 package com.jldeveloper.dslist.services;
 
+import com.jldeveloper.dslist.dto.GameMinDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GameService {
+
+    public List<GameMinDTO> findAll() {
+
+    }
 
 }
