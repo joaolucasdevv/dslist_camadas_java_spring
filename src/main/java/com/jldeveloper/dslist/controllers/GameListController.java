ab2aba0b79cd97b6442ea1961ec5389daf616c23
Joao Lucas <jldecristo@gmail.com>
2025-05-24 16:27:05 -0300

    implementação do controller

--- src/main/java/com/jldeveloper/dslist/controllers/GameListController.java
+++ src/main/java/com/jldeveloper/dslist/controllers/GameListController.java
@@ -1,32 +1,24 @@
 package com.jldeveloper.dslist.controllers;
 
-import com.jldeveloper.dslist.dto.GameDTO;
-import com.jldeveloper.dslist.dto.GameMinDTO;
-import com.jldeveloper.dslist.services.GameService;
+import com.jldeveloper.dslist.dto.GameListDTO;
+import com.jldeveloper.dslist.services.GameListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/games")
+@RequestMapping(value = "/lists")
 public class GameListController {
 
     @Autowired
-    private GameService gameService;
-
-    @GetMapping(value = "/{id}")
-    public GameDTO findById(@PathVariable Long id) {
-        GameDTO result = gameService.findById(id);
-        return result;
-    }
+    private GameListService gameListService;
 
     @GetMapping
-    public List<GameMinDTO> findAll() {
-        List<GameMinDTO> result = gameService.findAll();
+    public List<GameListDTO> findAll() {
+        List<GameListDTO> result = gameListService.findAll();
         return result;
     }
 }
