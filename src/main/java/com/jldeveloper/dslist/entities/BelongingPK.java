f4913150441b943986eb199928ee52371d737714
Joao Lucas <jldecristo@gmail.com>
2025-05-24 11:57:44 -0300

    belongingPK construtcors, getters and setters, equals hashcode

--- src/main/java/com/jldeveloper/dslist/entities/BelongingPK.java
+++ src/main/java/com/jldeveloper/dslist/entities/BelongingPK.java
@@ -1,8 +1,45 @@
 package com.jldeveloper.dslist.entities;
 
+import java.util.Objects;
+
 public class BelongingPK {
 
     private Game game;
     private GameList list;
 
+    public BelongingPK() {
+    }
+
+    public BelongingPK(Game game, GameList list) {
+        this.game = game;
+        this.list = list;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public GameList getList() {
+        return list;
+    }
+
+    public void setList(GameList list) {
+        this.list = list;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BelongingPK that = (BelongingPK) o;
+        return Objects.equals(game, that.game) && Objects.equals(list, that.list);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(game, list);
+    }
 }
