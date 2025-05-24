9bdc65479019b30e1baeba9d39700d2e5e9669de
Joao Lucas <jldecristo@gmail.com>
2025-05-24 16:07:19 -0300

    Propriedades, constructors, getters e setters do GameListDTO

--- src/main/java/com/jldeveloper/dslist/dto/GameListDTO.java
+++ src/main/java/com/jldeveloper/dslist/dto/GameListDTO.java
@@ -1,4 +1,26 @@
 package com.jldeveloper.dslist.dto;
+import com.jldeveloper.dslist.entities.GameList;
+import org.springframework.beans.BeanUtils;
 
 public class GameListDTO {
+
+    private Long id;
+    private String name;
+
+    public GameListDTO() {
+    }
+
+    public GameListDTO(GameList entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
