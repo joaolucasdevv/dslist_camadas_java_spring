234f7918fb475ea1bf213f7e242dacdf7118b2d4
Joao Lucas <jldecristo@gmail.com>
2025-05-24 16:09:55 -0300

    listrepository

---
+++ src/main/java/com/jldeveloper/dslist/repositories/ListRepository.java
@@ -0,0 +1,8 @@
+package com.jldeveloper.dslist.repositories;
+
+import com.jldeveloper.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ListRepository extends JpaRepository<Game, Long> {
+
+}
