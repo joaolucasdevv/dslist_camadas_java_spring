ca18c48e43bcea49eb00facb610a1fb384e88389
Joao Lucas <jldecristo@gmail.com>
2025-05-24 16:20:55 -0300

    GameListController

---
+++ src/main/java/com/jldeveloper/dslist/controllers/GameListController.java
@@ -0,0 +1,32 @@
+package com.jldeveloper.dslist.controllers;
+
+import com.jldeveloper.dslist.dto.GameDTO;
+import com.jldeveloper.dslist.dto.GameMinDTO;
+import com.jldeveloper.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameListController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+}
