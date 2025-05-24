61e6adfaecca6a84fdcb3df4509f5376859ddc7e
Joao Lucas <jldecristo@gmail.com>
2025-05-24 15:03:22 -0300

    propriedades e copyProperties da entity

--- src/main/java/com/jldeveloper/dslist/dto/GameDTO.java
+++ src/main/java/com/jldeveloper/dslist/dto/GameDTO.java
@@ -1,4 +1,28 @@
 package com.jldeveloper.dslist.dto;
 
+import com.jldeveloper.dslist.entities.Game;
+import org.springframework.beans.BeanUtils;
+
+
 public class GameDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public GameDTO() {
+
+    }
+
+    public GameDTO(Game entity) {
+        BeanUtils.copyProperties(entity, this);
+    }
+
+
 }
