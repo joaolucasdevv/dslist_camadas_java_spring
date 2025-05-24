101fe3fafa52bd1f4e6aeaaa048908890d9c10fd
Joao Lucas <jldecristo@gmail.com>
2025-05-24 12:10:14 -0300

    Belonging @entity @table

--- src/main/java/com/jldeveloper/dslist/entities/Belonging.java
+++ src/main/java/com/jldeveloper/dslist/entities/Belonging.java
@@ -1,12 +1,15 @@
 package com.jldeveloper.dslist.entities;
 
 import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_belonging")
 public class Belonging {
 
     @EmbeddedId
     private BelongingPK id = new BelongingPK();
 
     private Integer position;
-
 }
