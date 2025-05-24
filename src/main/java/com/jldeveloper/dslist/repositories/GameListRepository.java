c31111d85d5fdeca3dd355dd13f40026e7675cdb
Joao Lucas <jldecristo@gmail.com>
2025-05-24 16:18:46 -0300

    GameListService

--- src/main/java/com/jldeveloper/dslist/repositories/GameListRepository.java
+++ src/main/java/com/jldeveloper/dslist/repositories/GameListRepository.java
@@ -3,6 +3,6 @@ package com.jldeveloper.dslist.repositories;
 import com.jldeveloper.dslist.entities.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ListRepository extends JpaRepository<GameList, Long> {
+public interface GameListRepository extends JpaRepository<GameList, Long> {
 
 }
--- src/main/java/com/jldeveloper/dslist/services/GameListService.java
+++ src/main/java/com/jldeveloper/dslist/services/GameListService.java
@@ -1,9 +1,8 @@
 package com.jldeveloper.dslist.services;
 
-import com.jldeveloper.dslist.dto.GameDTO;
-import com.jldeveloper.dslist.dto.GameMinDTO;
-import com.jldeveloper.dslist.entities.Game;
-import com.jldeveloper.dslist.repositories.GameRepository;
+import com.jldeveloper.dslist.dto.GameListDTO;
+import com.jldeveloper.dslist.entities.GameList;
+import com.jldeveloper.dslist.repositories.GameListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,17 +13,11 @@ import java.util.List;
 public class GameListService {
 
     @Autowired
-    private GameRepository gameRepository;
+    private GameListRepository gameListRepository;
 
     @Transactional(readOnly = true)
-    public GameDTO findById(Long id) {
-        Game result = gameRepository.findById(id).get();
-        return new GameDTO(result);
-    }
-
-    @Transactional(readOnly = true)
-    public List<GameMinDTO> findAll() {
-        List<Game> result = gameRepository.findAll();
-        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    public List<GameListDTO> findAll() {
+        List<GameList> result = gameListRepository.findAll();
+        return result.stream().map(x -> new GameListDTO(x)).toList();
     }
 }
