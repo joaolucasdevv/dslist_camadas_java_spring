ac49415cf40dee45597e04ad735c256aa2cf6d3a
Joao Lucas <jldecristo@gmail.com>
2025-05-24 12:24:58 -0300

    Mapeando a classe gamelist

--- src/main/java/com/jldeveloper/dslist/entities/GameList.java
+++ src/main/java/com/jldeveloper/dslist/entities/GameList.java
@@ -1,7 +1,13 @@
 package com.jldeveloper.dslist.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 import java.util.Objects;
 
+
+@Entity
+@Table(name = "tb_gamelist")
 public class GameList {
 
     private Long id;
