08eeef712c135d5aafdd7c58a9ae929826d6982c
Joao Lucas <jldecristo@gmail.com>
2025-05-25 20:44:46 -0300

    alteração da posição do game na lista

--- src/main/java/com/jldeveloper/dslist/services/GameListService.java
+++ src/main/java/com/jldeveloper/dslist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.jldeveloper.dslist.services;
 
 import com.jldeveloper.dslist.dto.GameListDTO;
 import com.jldeveloper.dslist.entities.GameList;
+import com.jldeveloper.dslist.projections.GameMinProjection;
 import com.jldeveloper.dslist.repositories.GameListRepository;
+import com.jldeveloper.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,9 +17,19 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(x -> new GameListDTO(x)).toList();
     }
+
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+    }
+
 }
