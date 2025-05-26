e84e8a5e1bf5a86e0bb61489f5e0da7441cfe94f
Joao Lucas <jldecristo@gmail.com>
2025-05-25 21:05:15 -0300

    implementação do ReplacementDTO

--- src/main/java/com/jldeveloper/dslist/controllers/GameListController.java
+++ src/main/java/com/jldeveloper/dslist/controllers/GameListController.java
@@ -2,13 +2,11 @@ package com.jldeveloper.dslist.controllers;
 
 import com.jldeveloper.dslist.dto.GameListDTO;
 import com.jldeveloper.dslist.dto.GameMinDTO;
+import com.jldeveloper.dslist.dto.ReplacementDTO;
 import com.jldeveloper.dslist.services.GameListService;
 import com.jldeveloper.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +30,10 @@ public class GameListController {
     public List<GameMinDTO> findByList(@PathVariable Long listId) {
         List<GameMinDTO> result = gameService.findByList(listId);
         return result;
+    }
+
+    @PostMapping(value = "/{listId}/games")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 }
